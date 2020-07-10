29d3d859c8d61405c93bc6b8f93e6c2b9faebab6
Bruno Futema <bruno.futema@outlook.com>
2020-07-10 06:12:14 -0300

    feat(backend): add StorageProvider and configuring jest

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,8 @@
 import { container } from 'tsyringe';
 
+import '@modules/users/providers';
+import './providers';
+
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
@@ -8,10 +11,10 @@ import UsersAccountsStatusRepository from '@modules/users/infra/typeorm/reposito
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
-  UsersRepository
+  UsersRepository,
 );
 
 container.registerSingleton<IUsersAccountsStatusRepository>(
   'UsersAccountsStatusRepository',
-  UsersAccountsStatusRepository
+  UsersAccountsStatusRepository,
 );
