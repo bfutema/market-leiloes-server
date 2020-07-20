55434a1076b75f17e4feb90f1d15a5991278d53e
Bruno Futema <bruno.futema@outlook.com>
2020-07-20 19:18:52 -0300

    chore(backend): overriting Express and Multer.File types

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,7 +1,16 @@
 declare namespace Express {
+  declare namespace Multer {
+    export interface File {
+      key: string;
+      location: string;
+    }
+  }
+
   export interface Request {
     user: {
       id: string;
     };
+
+    file: Multer.File;
   }
 }
